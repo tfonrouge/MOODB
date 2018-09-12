2081ae0e523f8a81a001e8a19ea709fbc9dc313b
tfonrouge <tfonrouge@gmail.com>
2018-09-12 13:25:47 -0500

    * moved logic for assigning values on linkedTable fields to UI

--- src/tech/fonrouge/MOODB/MFieldTableField.java
+++ src/tech/fonrouge/MOODB/MFieldTableField.java
@@ -4,12 +4,15 @@ import org.bson.types.ObjectId;
 
 public abstract class MFieldTableField<T extends MTable> extends MFieldObject {
 
-    public final T tableField = initializeTableField();
-    private MFieldTableField<? extends MTable> linkedField;
-    private String expression;
+    private final T linkedTable = initializeTableField();
+    private MFieldTableField<T> linkedField;
 
     protected MFieldTableField(MTable owner, String name) {
         super(owner, name);
+    }
+
+    public T getLinkedTable() {
+        return linkedTable;
     }
 
     private T initializeTableField() {
@@ -31,83 +34,18 @@ public abstract class MFieldTableField<T extends MTable> extends MFieldObject {
     }
 
     @SuppressWarnings("unused")
-    public String getExpression() {
-        return expression;
-    }
-
-    @SuppressWarnings("unused")
-    public void setExpression(String expression) {
-        this.expression = expression;
-    }
-
-    @SuppressWarnings("unused")
-    public String[] getExpressionList() {
-        if (expression != null) {
-            return expression.split("\\.");
-        }
-        return null;
-    }
-
-    @SuppressWarnings("unused")
-    public Object getValueFromExpression() {
-        return getValueFromExpression(expression);
-    }
-
-    public Object getValueFromExpression(String expression) {
-        Object value = tableField()._id();
-        if (expression != null) {
-            int nPos = expression.indexOf('.');
-            if (nPos > 0) {
-                String fieldName = expression.substring(nPos + 1);
-                MField mField = tableField.getFieldList().get(fieldName);
-                if (mField != null) {
-                    value = mField.value();
-                } else {
-                    value = "Error on: '" + fieldName + "'";
-                }
-            }
-        }
-        return value;
-    }
-
-    @SuppressWarnings("unused")
     public boolean setValue(T table) {
         return super.setValue(table._id());
     }
 
-    @SuppressWarnings("unused")
-    public boolean setValueWithExpression(String value) {
-        return setValueWithExpression(expression, value);
-    }
+    public final T syncLinkedTable() {
 
-    public boolean setValueWithExpression(String expression, String value) {
-        if (expression != null) {
-            int nPos = expression.indexOf('.');
-            if (nPos > 0) {
-                String fieldName = expression.substring(nPos + 1);
-                MFieldString mField = (MFieldString) tableField.getFieldList().get(fieldName);
-                if (mField != null) {
-                    if (value == null || value.isEmpty()) {
-                        return super.setValue(null);
-                    } else {
-                        if (mField.find(value)) {
-                            return setValue(tableField);
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public final T tableField() {
-
-        Object objectId = tableField._id();
+        Object objectId = linkedTable._id();
 
         if (objectId == null || !objectId.equals(value)) {
-            tableField.goTo(value);
+            linkedTable.goTo(value);
         }
 
-        return tableField;
+        return linkedTable;
     }
 }
