50757607ca43df97a5cd552013b3f3b59f31ea1a
tfonrouge <tfonrouge@gmail.com>
2018-11-22 21:29:26 -0600

    + setValue

--- src/tech/fonrouge/MOODB/MBaseData.java
+++ src/tech/fonrouge/MOODB/MBaseData.java
@@ -40,7 +40,6 @@ public abstract class MBaseData {
         private MBaseData MBaseData;
         private String label;
         private boolean isCalculated;
-
         DataValueItem(MField mField) {
             isCalculated = mField.isCalculated();
             value = mField.value();
@@ -63,6 +62,10 @@ public abstract class MBaseData {
 
         public Object getValue() {
             return value;
+        }
+
+        public void setValue(Object value) {
+            this.value = value;
         }
 
         public boolean isTableField() {
