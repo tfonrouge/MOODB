b3f693676a614de0c1125cb6767446ef8a87bed9
tfonrouge <tfonrouge@gmail.com>
2019-03-23 21:05:52 -0600

    * getNextSequence() for autoInc fields (Integer)

--- src/tech/fonrouge/MOODB/MFieldInteger.java
+++ src/tech/fonrouge/MOODB/MFieldInteger.java
@@ -1,5 +1,10 @@
 package tech.fonrouge.MOODB;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
+import org.bson.Document;
+
 public class MFieldInteger extends MField<Integer> {
 
     public MFieldInteger(MTable owner, String name) {
@@ -36,6 +41,26 @@ public class MFieldInteger extends MField<Integer> {
                 return Integer.valueOf((String) anyValue);
         }
         return null;
+    }
+
+    @Override
+    public Integer getNextSequence() {
+        MongoCollection<Document> countersCollection = table.engine.getMongoDatabase().getCollection("__counters");
+
+        Document query = new Document("_id", table.getTableName());
+        Document update = new Document("$inc", new Document(name, 1));
+
+        FindOneAndUpdateOptions options = new FindOneAndUpdateOptions();
+        options.upsert(true);
+        options.returnDocument(ReturnDocument.AFTER);
+
+        Document result = countersCollection.findOneAndUpdate(query, update, options);
+
+        if (result == null) {
+            return 0;
+        }
+
+        return result.getInteger(name);
     }
 
     @Override
