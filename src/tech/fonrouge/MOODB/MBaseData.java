0d0970e81231d997c75048ec4bb6313c5367de81
tfonrouge <tfonrouge@gmail.com>
2019-03-23 21:07:11 -0600

    Update MBaseData.java

--- src/tech/fonrouge/MOODB/MBaseData.java
+++ src/tech/fonrouge/MOODB/MBaseData.java
@@ -1,83 +1,22 @@
 package tech.fonrouge.MOODB;
 
-import java.util.HashMap;
+import org.bson.Document;
 
 public abstract class MBaseData {
 
-    public final HashMap<String, DataValueItem> fieldValues;
     protected final MTable table;
+    protected Document document;
 
     public MBaseData(MTable base) {
-
         table = base;
-        fieldValues = new HashMap<>();
-
         populateFieldValues();
-
     }
 
     private void populateFieldValues() {
-        table.getFieldListStream().forEach(mField -> {
-            fieldValues.put(mField.getName(), new DataValueItem(mField));
-        });
-    }
-
-    public DataValueItem getDataValueItem(String fieldName) {
-        return fieldValues.get(fieldName);
-    }
-
-    public Object fieldValue(String fieldName) {
-        return fieldValues.get(fieldName).getValue();
+        document = table.getDocument();
     }
 
     public Object get_id() {
-        return fieldValue("_id");
-    }
-
-    public class DataValueItem {
-        private Object value;
-        private boolean isTableField;
-        private MBaseData MBaseData;
-        private String label;
-        private boolean isCalculated;
-        DataValueItem(MField mField) {
-            isCalculated = mField.isCalculated();
-            value = mField.value();
-            isTableField = mField instanceof MFieldTableField;
-            label = mField.getLabel();
-            if (isTableField) {
-                MFieldTableField mFieldTableField = (MFieldTableField) mField;
-                MTable base = mFieldTableField.getLinkedTable();
-                MBaseData = base.getData();
-            }
-        }
-
-        public boolean isCalculated() {
-            return isCalculated;
-        }
-
-        public String getLabel() {
-            return label;
-        }
-
-        public Object getValue() {
-            return value;
-        }
-
-        public void setValue(Object value) {
-            this.value = value;
-        }
-
-        public boolean isTableField() {
-            return isTableField;
-        }
-
-        public MBaseData getMBaseData() {
-            if (value == null || !value.equals(MBaseData.get_id())) {
-                MBaseData.table.field__id.find(value);
-                MBaseData.populateFieldValues();
-            }
-            return MBaseData;
-        }
+        return document.get("_id");
     }
 }
