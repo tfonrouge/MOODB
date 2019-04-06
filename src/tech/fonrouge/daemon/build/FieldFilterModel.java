d2c4ecc13be43fd26061cfcf890fc66c730740ae
tfonrouge <tfonrouge@gmail.com>
2019-04-06 15:39:31 -0600

    + filter model

---
+++ src/tech/fonrouge/daemon/build/FieldFilterModel.java
@@ -0,0 +1,18 @@
+package tech.fonrouge.daemon.build;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+class FieldFilterModel {
+
+    String fieldName;
+    String filterValue;
+
+    FieldFilterModel(Node node) {
+        fieldName = node.getAttributes().getNamedItem("field").getNodeValue();
+        NodeList childNodes = node.getChildNodes();
+        if (childNodes != null && childNodes.getLength()>0) {
+            filterValue = childNodes.item(0).getTextContent();
+        }
+    }
+}
