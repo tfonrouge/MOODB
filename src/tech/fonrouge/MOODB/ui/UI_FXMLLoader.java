d3958b5c8ad6eac0e3207e86349b75a5aeff0885
Teo Fonrouge <teo@fonrouge.com>
2019-06-25 00:36:54 -0500

    * on binding assign UI_CtrlList field value (if declared) with *Control

--- src/tech/fonrouge/MOODB/ui/UI_FXMLLoader.java
+++ src/tech/fonrouge/MOODB/ui/UI_FXMLLoader.java
@@ -1,17 +1,19 @@
 package tech.fonrouge.MOODB.ui;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import tech.fonrouge.MOODB.Annotations.BindCtrlList;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
 
+@SuppressWarnings("unused")
 public class UI_FXMLLoader {
 
     @SuppressWarnings("unused")
     public static void fxmlLoadBindCtrlList(FXMLLoader fxmlLoader) {
         try {
-            Object controller = fxmlLoader.load();
+            Parent controller = fxmlLoader.load();
             if (UI_CtrlList.currentCtrlList != null) {
                 setControllerMember(controller, UI_CtrlList.currentCtrlList);
             }
@@ -21,20 +23,37 @@ public class UI_FXMLLoader {
     }
 
     @SuppressWarnings("unused")
-    public static void fxmlLoadBindCtrlList(FXMLLoader fxmlLoader, Object controller) {
+    public static void fxmlLoadBindCtrlList(FXMLLoader fxmlLoader, Parent parent) {
         try {
             if (UI_CtrlList.currentCtrlList != null) {
-                setControllerMember(controller, UI_CtrlList.currentCtrlList);
+                setControllerMember(parent, UI_CtrlList.currentCtrlList);
             }
             fxmlLoader.load();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        String id = parent.getId();
+        if (id != null && !id.isEmpty()) {
+            injectFieldValue(UI_CtrlList.currentCtrlList, parent, id);
+        }
     }
 
-    private static void setControllerMember(Object controller, UI_CtrlList last_ui_ctrlList) {
+    private static void injectFieldValue(UI_CtrlList ui_ctrlList, Parent parent, String id) {
+        Class<?> clazz = ui_ctrlList.getClass();
+        while (clazz != UI_CtrlList.class) {
+            for (Field field : clazz.getDeclaredFields()) {
+                if (field.getName().contentEquals(id) && field.getType().isAssignableFrom(parent.getClass())) {
+                    setFieldValue(field, ui_ctrlList, parent);
+                    return;
+                }
+            }
+            clazz = clazz.getSuperclass();
+        }
+    }
+
+    private static void setControllerMember(Parent parent, UI_CtrlList ui_ctrlList) {
         Field field = null;
-        for (Field declaredField : controller.getClass().getDeclaredFields()) {
+        for (Field declaredField : parent.getClass().getDeclaredFields()) {
             BindCtrlList annotation = declaredField.getAnnotation(BindCtrlList.class);
             if (annotation != null && UI_CtrlList.class.isAssignableFrom(declaredField.getType())) {
                 field = declaredField;
@@ -42,18 +61,21 @@ public class UI_FXMLLoader {
             }
         }
         if (field != null) {
-            try {
-                boolean accesible = field.isAccessible();
-                if (!accesible) {
-                    field.setAccessible(true);
-                }
-                field.set(controller, last_ui_ctrlList);
-                if (!accesible) {
-                    field.setAccessible(false);
-                }
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
+            setFieldValue(field, parent, ui_ctrlList);
+        }
+    }
+
+    private static void setFieldValue(Field field, Object obj, Object value) {
+        boolean accesible = field.isAccessible();
+        if (!accesible) {
+            field.setAccessible(true);
+        }
+        try {
+            field.set(obj, value);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } finally {
+            field.setAccessible(accesible);
         }
     }
 }
